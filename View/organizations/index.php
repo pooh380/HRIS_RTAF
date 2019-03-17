96096e9a699df8957a6a6dec635a015ebaf52d87
pooh <poohivaoth@gmail.com>
2019-03-17 21:19:20 +0700

    ทำไฟล์ template header

--- View/organizations/index.php
+++ View/organizations/index.php
@@ -1,50 +1,24 @@
-<?php
-  require_once '../../dbconfig.php';
-  include_once '../../libs/Medoo.php';
-?>
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+  <head>
+    <title>Organizations</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<script
-  src="http://code.jquery.com/jquery-3.3.1.js"
-  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-  crossorigin="anonymous"></script>
-<script src="../../Controller/organizationsController.js"></script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
 
-  
-    
-
-    <h1>ดีจ้า454564566545646556</h1>
-    <h2>454654564654</h2>
-    
-    <div id='open_ender_output'></div>
-    <button type="button" onclick="myFunction()">ส่งค่าตัวแปร</button>
-    <div id='a'></div>
-
-    
-
-    <form method="post" name="postForm">
-    <ul>
-        <li>
-            <label>Name</label>
-            <input type="text" name="name" id="name" placeholder="Bruce Wayne">
-            <span class="throw_error"></span>
-            <span id="success"></span>
-       </li>
-      </ul>
-      <input type="submit" value="Send" onclick="addOrganizations()"/>
-    </form>
-
-    <div id='testAjax'></div>
-
-
-</body>
+    <header>
+      <?php include '../../include/header.php'; ?>
+    </header>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
 </html>
---
+++ include/header.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Organizations</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+    <!-- <link rel="stylesheet" href="./Asset/Css/header.css"> -->
+
+    <style>
+    @import url(../../Asset/Css/thsarabunnew.css);
+
+    /* Desktop Styles */
+
+    @media (min-width: 1200px) {
+        header {
+            width: 100%;
+            height: 75px;
+            /* background-color: aqua; */
+            background-image: url(../../Asset/Images/header.jpg);
+        }
+        #hamberger {
+            float: left;
+            padding-top: 20px;
+            margin-left: 20px;
+        }
+        #hris {
+            padding-top: 15px;
+            margin-left: 10px;
+            /* border: 3px solid #ddafcb; */
+            width: 400px;
+            float: left;
+        }
+        #user {
+            /* border: 3px solid #bacdfa; */
+            padding-top: 20px;
+            margin-right: 20px;
+            width: auto;
+        }
+        .button {
+            color: #222233;
+            font-family: 'THSarabunNew', sans-serif;
+            font-size: 16px;
+            font-weight: bold;
+            border-radius: 19px;
+        }
+    }
+    </style>
+  
+  </head>
+  <body>
+    <header>
+        <div id="hamberger">
+            <button type="button" class="btn btn-primary">
+                <i class="fas fa-bars"></i>
+            </button>
+        </div>
+        <div id="hris">
+            <img src="../../Asset/Images/hris.png" alt="hris" width="240px;" height="45px;">
+        </div>
+        <div id="user" align="right">
+            <button type="button" class="button btn btn-light">
+                TEST
+                <i class="fas fa-user-circle"></i>
+                <i class="fas fa-angle-right"></i>
+            </button>
+        </div>
+
+    </header>
+
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
