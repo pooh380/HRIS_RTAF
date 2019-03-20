cdb77150b07c6b46a2cfb2594eae34ecca598f27
pooh <poohivaoth@gmail.com>
2019-03-20 19:39:33 +0700

    1.แก้ไฟล์ header footer ให้ link กับ datatable ให้ได้ 2.ไฟล์ test datatable hris_rtaf/Views/Prefixs/datatable.php 3.หน้า index หลักที่มี datatable hris_rtaf/Views/Prefixs/index.php

--- Views/Prefixs/index.php
+++ Views/Prefixs/index.php
@@ -4,7 +4,6 @@
   <!-- menu -->
   <?php include '../include/menu.php'; ?>
 
-  
   <section>
   <div class="app-content content">
     <div class="content-wrapper">
@@ -14,106 +13,139 @@
         </div>
       </div>
       <div class="content-body">
-        <!-- Description -->
-        <section id="description" class="card">
-          <div class="card-header">
-            <h4 class="card-title">Description</h4>
-          </div>
-          <div class="card-content">
-            <div class="card-body">
-              <div class="card-text">
-                <p>Modern Admin provides flexible navbar component, which allows you
-                  to modify navbar colors, appearance of different types of content,
-                  components placement etc. This page contain all navbar styling
-                  options.
-                </p>
-
-                <div class="table-responsive">
-                  <table class="table table-hover">
-                    <thead>
-                      <tr>
-                        <th>Classes</th>
-                        <th>Description</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <th scope="row"><code>.fixed-top</code></th>
-                        <td>To set navbar fixed at top you need to add <code>.fixed-top</code>                          class in navbar <code>&lt;nav&gt;</code> tag. For more
-                          information please check <a href="fixed-top.html" target="_blank">fixed-top.html</a>.</td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><code>.fixed-bottom</code></th>
-                        <td>To set navbar fixed at bottom you need to add <code>.fixed-bottom</code>                          class in navbar <code>&lt;nav&gt;</code> tag.</td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><code>.navbar-static-top</code></th>
-                        <td>Create a full-width navbar that scrolls away with the page
-                          by adding <code>.navbar-static-top</code>. Unlike the <code>.navbar-fixed-*</code>                          classes, you do not need to change any padding on the <code>body</code>.</td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><code>.navbar-brand-center</code></th>
-                        <td>This class allow you to set navbar branding portion at center.
-                          Add <code>.navbar-brand-center</code> class in navbar <code>&lt;nav&gt;</code>                          tag. For more information please check <a href="navbar-brand-center.html"
-                          target="_blank">navbar-brand-center.html</a>. This is optional
-                          class.
-                        </td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><code>.navbar-light</code></th>
-                        <td> To set navbar light color you need to add <code>.navbar-light</code>                          class in navbar <code>&lt;nav&gt;</code> tag. For more
-                          information please check <a href="navbar-light.html" target="_blank">navbar-light.html</a></td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><code>.navbar-dark</code></th>
-                        <td> To set navbar dark color you need to add <code>.navbar-dark</code>                          class in navbar <code>&lt;nav&gt;</code> tag. For more
-                          information please check <a href="navbar-dark.html" target="_blank">navbar-dark.html</a></td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><code>.navbar-semi-dark</code></th>
-                        <td> To set navbar semi dark color you need to add <code>.navbar-semi-dark</code>                          class in navbar <code>&lt;nav&gt;</code> tag. For more
-                          information please check <a href="navbar-semi-dark.html"
-                          target="_blank">navbar-semi-dark.html</a></td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><code>.navbar-border</code></th>
-                        <td>You can add navbar border bottom by adding <code>.navbar-border</code>                          class in navbar <code>&lt;nav&gt;</code> tag. This navbar
-                          class is optional.</td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><code>.navbar-shadow</code></th>
-                        <td>You can also apply navbar shadow bottom by adding <code>.navbar-shadow</code>                          class in navbar <code>&lt;nav&gt;</code> tag. This navbar
-                          class is optional.</td>
-                      </tr>
-                      <tr>
-                        <th scope="row"><code>.disabled</code></th>
-                        <td>To disabled navbar menu item link, you need to add <code>.disabled</code>                          class in navigation
-                          <code>&lt;li&gt;</code> tag. This navigation class is optional.
-                          Fore more information please check <a href="vertical-disabled-navigation-link.html"
-                          target="_blank">vertical-disabled-navigation-link.html</a></td>
-                      </tr>
-                    </tbody>
-                  </table>
+        <!-- Checkbox selection -->
+        <section id="checkbox">
+          <div class="row">
+            <div class="col-12">
+              <div class="card">
+                <!-- <div class="card-header"> -->
+                  <!-- <h4 class="card-title">Checkbox selection</h4> -->
+                  <!-- <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a> -->
+                  <!-- <div class="heading-elements">
+                    <ul class="list-inline mb-0">
+                      <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                      <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                      <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                      <li><a data-action="close"><i class="ft-x"></i></a></li>
+                    </ul>
+                  </div> -->
+                <!-- </div> -->
+                <div class="card-content collapse show">
+                  <div class="card-body card-dashboard">
+                    <p class="card-text"></p>
+                    <table class="table table-striped table-bordered responsive dataex-select-checkbox" style="width:100%;">
+                      <thead>
+                        <tr>
+                          <th></th>
+                          <th>Name</th>
+                          <th>Position</th>
+                          <th>Office</th>
+                          <th>Age</th>
+                          <th>Salary</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td></td>
+                          <td>Cara Stevens</td>
+                          <td>Sales Assistant</td>
+                          <td>New York</td>
+                          <td>46</td>
+                          <td>$145,600</td>
+                        </tr>
+                        <tr>
+                          <td></td>
+                          <td>Hermione Butler</td>
+                          <td>Regional Director</td>
+                          <td>London</td>
+                          <td>47</td>
+                          <td>$356,250</td>
+                        </tr>
+                        <tr>
+                          <td></td>
+                          <td>Lael Greer</td>
+                          <td>Systems Administrator</td>
+                          <td>London</td>
+                          <td>21</td>
+                          <td>$103,500</td>
+                        </tr>
+                        <tr>
+                          <td></td>
+                          <td>Jonas Alexander</td>
+                          <td>Developer</td>
+                          <td>San Francisco</td>
+                          <td>30</td>
+                          <td>$86,500</td>
+                        </tr>
+                        <tr>
+                          <td></td>
+                          <td>Shad Decker</td>
+                          <td>Regional Director</td>
+                          <td>Edinburgh</td>
+                          <td>51</td>
+                          <td>$183,000</td>
+                        </tr>
+                        <tr>
+                          <td></td>
+                          <td>Michael Bruce</td>
+                          <td>Javascript Developer</td>
+                          <td>Singapore</td>
+                          <td>29</td>
+                          <td>$183,000</td>
+                        </tr>
+                        <tr>
+                          <td></td>
+                          <td>Donna Snider</td>
+                          <td>Customer Support</td>
+                          <td>New York</td>
+                          <td>27</td>
+                          <td>$112,000</td>
+                        </tr>
+                      </tbody>
+                      <tfoot>
+                        <tr>
+                          <th></th>
+                          <th>Name</th>
+                          <th>Position</th>
+                          <th>Office</th>
+                          <th>Age</th>
+                          <th>Salary</th>
+                        </tr>
+                      </tfoot>
+                    </table>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </section>
-        <!--/ Description -->
+        <!-- Checkbox selection -->
       </div>
     </div>
   </div>
   </section>
-  <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  
+  <!-- datatables -->
+  <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+  <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+  
+    <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  
+  
+
   <script type="text/javascript">
     $(document).ready(function() {
             console.log("ready");
-            change_autorefreshdiv()
+            change_autorefreshdiv();
     });
+
     function change_autorefreshdiv(){
       // $('#prefixPage').addClass('active');
 }   
   </script>
+
+    
 
   <!-- footer -->
   <?php include '../include/footer.php'; ?>
