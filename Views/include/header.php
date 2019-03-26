46ec528c9cbe298e964d9d361ca17ab95e49da59
pooh <poohivaoth@gmail.com>
2019-03-26 14:21:43 +0700

    no message

--- Views/include/header.php
+++ Views/include/header.php
@@ -50,7 +50,7 @@
                     <li class="nav-item mobile-menu d-md-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ft-menu font-large-1"></i></a></li>
 
                     <li class="nav-item">
-                        <a class="navbar-brand" href="index.html">
+                        <a class="navbar-brand" href="./../home/index.php">
                             <img class="brand-logo" alt="modern admin logo" src="../../Asset/Images/Emblem.png" style="float:left;">
                             <div>
                                 <h3 class="brand-text" style="color:#fecd01;">กองทัพอากาศ</h3> <br>
