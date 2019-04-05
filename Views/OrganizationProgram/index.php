b1445cc177ac06a0f9a8a3620723d756059f67ab
pooh <poohivaoth@gmail.com>
2019-04-05 19:00:52 +0700

    select2 ดึงข้อมูล db อ๋องต้องดูจากหน้านี้เพราะอ๋องทำ

--- Views/OrganizationProgram/index.php
+++ Views/OrganizationProgram/index.php
@@ -6,6 +6,85 @@
 <!-- menu -->
 <?php include '../include/menu.php'; ?>
 
+<!-- BEGIN VENDOR CSS-->
+<link rel="stylesheet" type="text/css" href="../../app-assets/css/vendors.css">
+<link rel="stylesheet" type="text/css" href="../../app-assets/vendors/css/forms/selects/select2.min.css">
+<!-- END VENDOR CSS-->
+<!-- BEGIN MODERN CSS-->
+<link rel="stylesheet" type="text/css" href="../../app-assets/css/app.css">
+<!-- END MODERN CSS-->
+<!-- BEGIN Page Level CSS-->
+<link rel="stylesheet" type="text/css" href="../../app-assets/css/core/menu/menu-types/vertical-menu.css">
+<link rel="stylesheet" type="text/css" href="../../app-assets/css/core/colors/palette-gradient.css">
+<!-- END Page Level CSS-->
+<!-- BEGIN Custom CSS-->
+<link rel="stylesheet" type="text/css" href="../../asset/css/style.css">
+<!-- END Custom CSS-->
+
+<script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.3.1.min.js"></script>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        console.log("ready");
+        // getOrganizationType();
+        $("#organizationType").select2({
+              ajax: {
+                url: "../../Model/OrganizationProgram/getOrganizationType.php",/* Url ที่ต้องการส่งค่าไปประมวลผลการค้นข้อมูล*/
+                dataType: 'json',
+                delay: 250,
+                data: function (params) {
+                    
+                  return {
+                    q: params.term, // ค่าที่ส่งไป
+                    page: params.page
+                  };
+ 
+                },
+                processResults: function (data, params) {
+                  // parse the results into the format expected by Select2
+                  // since we are using custom formatting functions we do not need to
+                  // alter the remote JSON data, except to indicate that infinite
+                  // scrolling can be used
+                  params.page = params.page || 1;
+ 
+                  return {
+                    results: data.items,
+                    pagination: {
+                      more: (params.page * 30) < data.total_count
+                    }
+                  };
+                },
+                cache: true
+              },
+              escapeMarkup: function (markup) { return markup; }, // let our custom formatter work
+              minimumInputLength: 1,
+              templateResult: formatRepo, // omitted for brevity, see the source of this page
+              templateSelection: formatRepoSelection // omitted for brevity, see the source of this page
+            });
+ 
+        });
+        
+ 
+        function formatRepo (repo) {
+        
+          if (repo.loading) return repo.text;
+          
+          var markup = "<div class='select2-result-repository clearfix'>" +
+            "<div class='select2-result-repository__meta'>" +
+              "<div class='select2-result-repository__title'>" + repo.value + "</div></div></div>";
+ 
+          return markup;
+        }
+ 
+        
+        function formatRepoSelection (repo) {
+          return repo.value || repo.text;
+        }
+
+</script>
+
+<script src="../../Controllers/OrganizationProgramController.js"></script>
+
 <section>
     <div class="app-content content">
         <div class="content-wrapper">
@@ -30,30 +109,33 @@
                                                 </div>
 
                                                 <fieldset class="form-group position-relative">
-
                                                     <div class="row">
-                                                        <div class="form-group col-lg-6 ">
-                                                            <label id="usernameForm" style=" font-weight:bold; font-size: 15px; color:#0f1733;">โครงสร้าง:</label>
-                                                            <select class="select2 form-control block" id="organization">
-                                                                <option value="โครงสร้างอัตราทอ.52">โครงสร้างอัตราทอ.52</option>
-                                                                <option value="โครงสร้างอื่นๆ">โครงสร้างอื่นๆ</option>
+
+
+                                                        <div class="form-group col-md-12">
+                                                            <label id="orgType" style=" font-weight:bold; font-size: 15px; color:#0f1733;">โครงสร้าง:</label>
+                                                            <select class="select2 form-control block" id="organizationType">
+                                                                <!-- <option value="โครงสร้างอัตราทอ.52">โครงสร้างอัตราทอ.52</option>
+                                                                    <option value="โครงสร้างอื่นๆ">โครงสร้างอื่นๆ</option> -->
+                                                                <option value="">กรุณาเลือกโครงสร้าง</option>
                                                             </select>
                                                         </div>
 
-                                                        <div class="form-group col-lg-6 ">
-                                                            <label id="passwordForm" style=" font-weight:bold; font-size: 15px;color:#0f1733;">หน่วยกำลังพล: </label>
+                                                        <div class="form-group col-md-12">
+                                                            <label id="usernameForm" style=" font-weight:bold; font-size: 15px; color:#0f1733;">หน่วย:</label>
                                                             <select class="select2 form-control block" id="group">
                                                                 <option value="โครงสร้างอัตราทอ.52">หน่วย</option>
                                                                 <option value="โครงสร้างอื่นๆ">โครงสร้างอื่นๆ</option>
                                                             </select>
-                                                        </div>
+                                                        </div> ก
                                                     </div>
                                                 </fieldset>
                                                 <div class="text-right">
                                                     <!-- <button type="submit" class="btn" onclick="search()" style="background-color:#0f1733;color:white;border:white"><i class="la la-search" style="margin-rifgt:8px"></i> -->
-                                                    <button type="submit" class="btn"    onclick="window.location.href='./create.php'" style="background-color:#0f1733;color:white;border:white"><i class="la la-search" style="margin-rifgt:8px"></i> 
-                                                        <span style=" font-weight:bold; font-size: 13px;"> ค้นหา</span></button>
-
+                                                    <a href="./detail.php">
+                                                        <button type="submit" class="btn" style="background-color:#0f1733;color:white;border:white"><i class="la la-search" style="margin-rifgt:8px"></i>
+                                                            <span style=" font-weight:bold; font-size: 13px;"> ค้นหา</span></button>
+                                                    </a>
 
                                                 </div>
 
@@ -72,8 +154,26 @@
     </div>
 </section>
 
+<footer class="footer footer-static footer-light navbar-border navbar-shadow">
+    <p class="clearfix blue-grey lighten-2 text-sm-center mb-0 px-2">
+        <span class="float-md-left d-block d-md-inline-block">Copyright &copy; 2018 <a class="text-bold-800 grey darken-2" href="https://themeforest.net/user/pixinvent/portfolio?ref=pixinvent" target="_blank">PIXINVENT </a>, All rights reserved. </span>
+        <span class="float-md-right d-block d-md-inline-blockd-none d-lg-block">Hand-crafted & Made with <i class="ft-heart pink"></i></span>
+    </p>
+</footer>
+<!-- BEGIN VENDOR JS-->
+<script src="../../app-assets/vendors/js/vendors.min.js" type="text/javascript"></script>
+<!-- BEGIN VENDOR JS-->
+<!-- BEGIN PAGE VENDOR JS-->
+<script src="../../app-assets/vendors/js/forms/select/select2.full.min.js" type="text/javascript"></script>
+<!-- END PAGE VENDOR JS-->
+<!-- BEGIN MODERN JS-->
+<script src="../../app-assets/js/core/app-menu.js" type="text/javascript"></script>
+<script src="../../app-assets/js/core/app.js" type="text/javascript"></script>
+<script src="../../app-assets/js/scripts/customizer.js" type="text/javascript"></script>
+<!-- END MODERN JS-->
+<!-- BEGIN PAGE LEVEL JS-->
+<script src="../../app-assets/js/scripts/forms/select/form-select2.js" type="text/javascript"></script>
+<!-- END PAGE LEVEL JS-->
+</body>
 
-<script src="../../Controllers/indexOrganizationsProgram"></script>
-<script src="http://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
-<!-- footer -->
-<?php include '../include/footer.php'; ?> 
+</html>
