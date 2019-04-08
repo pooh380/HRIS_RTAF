47fde85f6358f1993492b52ce97be6563a16e46f
pooh <poohivaoth@gmail.com>
2019-04-08 14:59:31 +0700

    Pass_Status เสร็จ

---
+++ Model/PassStatus/DeletePassStatus.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once "../../config.php";
+// if($conn){echo "con";}
+
+if (!empty($_POST)) {
+    // $originsId = 0;
+
+    $passStatusId = $_POST['passStatusId'];
+
+    // date_default_timezone_set('Asia/Bangkok');
+    // echo date("Y-m-d h:i:sa");
+
+
+    $sql = " DELETE FROM pass_status WHERE id=$passStatusId; ";
+    // echo $sql;
+
+    $query = sqlsrv_query($conn, $sql);
+
+    if ($query) {
+        echo "บันทึกสำเร็จ";
+    } else {
+        echo "บันทึกไม่ได้";
+    }
+}
+sqlsrv_close($conn);
