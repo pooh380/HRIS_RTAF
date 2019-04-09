f073cc10832a1d2cd3e905394cfcca2436d78804
pooh <poohivaoth@gmail.com>
2019-04-09 13:46:40 +0700

    new

--- Model/OrganizationProgram/getOrganizationType.php
+++ Model/OrganizationProgram/getOrganizationType.php
@@ -1,32 +1,3 @@
 <?php
 
 require_once "../../config.php";
-
-// $name = $_GET['q'];
-
-if (!empty($_GET['q'])) {
-    $sql = "SELECT id, organization_type_name FROM organization_type where organization_type_name  LIKE N'%" . $_GET['q'] . "%' ";
-    $result = sqlsrv_query($conn, $sql);
-} else { 
-    $sql = " SELECT id, organization_type_name FROM organization_type where organization_type_name ";
-    $result = sqlsrv_query($conn, $sql);
-}
-
-while ($row = sqlsrv_fetch_array($result)) {
-
-    $data[] =  array(
-        "id" => $row["id"],
-        "value" => $row["organization_type_name"]
-    );
-}
-
-$json = array(
-    "total" => "",
-    "items" => $data
-);
-
-
-echo json_encode($json);
-
-
-mysqli_close($conn);
