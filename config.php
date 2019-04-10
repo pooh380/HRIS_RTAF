dd4cef16326ba42c6e5c74eb6360b65eed91873e
pooh <poohivaoth@gmail.com>
2019-04-10 16:22:47 +0700

    Revert "no message"
    This reverts commit f0d3f7e3ebec1af4ed628452947c6b20db15f8ea.

--- config.php
+++ config.php
@@ -1,17 +1,17 @@
 
 <?php
-$serverName = "45.77.249.35"; //serverName\instanceName ถ้าฐานข้อมูลอยู่ในเครื่องเราใช้ localhost 
-// $serverName = "10.243.0.9";
+// $serverName = "45.77.249.35"; //serverName\instanceName ถ้าฐานข้อมูลอยู่ในเครื่องเราใช้ localhost 
+$serverName = "10.243.0.9/ACERSERVER2/MSSQLSERVER01";
 $connectionInfo = array(
     "Database" => "HRIS",
     "UID" => "sa",
-    "PWD" => "1020304050",
-    // "PWD" => "2123121254",
+    // "PWD" => "1020304050",
+    "PWD" => "2123121254",
     "MultipleActiveResultSets"=>true,
     "CharacterSet"  => 'UTF-8'
 );
  
-$conn = sqlsrv_connect( $serverName, $connectionInfo);
+$conn = sqlsrv_connect( $serverName, $connectionInfo,);
  
 if ( $conn ) {
     // echo "Connection established.<br />";
