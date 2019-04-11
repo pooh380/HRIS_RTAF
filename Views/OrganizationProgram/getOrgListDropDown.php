ccb13e0847d77910205c0bf6996ab8ca52f83281
pooh <poohivaoth@gmail.com>
2019-04-11 10:12:07 +0700

    orglistlist orgprogram

--- Views/OrganizationProgram/getOrgListDropDown.php
+++ Views/OrganizationProgram/getOrgListDropDown.php
@@ -3,15 +3,15 @@ function getOrgList()
 {
     require_once "../../config.php";
 
-
+    // $orgTypeId = $_POST['orgTypeId'];
+    $orgTypeId = 21;
+    $sql = " SELECT id,organization_list_name,organization_type_id,IsActive FROM organization_list WHERE organization_type_id = $orgTypeId and IsActive = 1 ; ";
+    $result = sqlsrv_query($conn, $sql);
     ?>
+
     <select name="orgListList" class="select2 form-control">
         <?php
-        if (!empty($_POST)) {
-
-            $orgTypeId = $_POST['orgTypeId'];
-            $sql = " SELECT id,organization_list_name FROM organization_list WHERE organization_type_id = $orgTypeId; ";
-            $result = sqlsrv_query($conn, $sql);
+        if (!empty($orgTypeId)) {
             while ($row = sqlsrv_fetch_array($result)) {
                 echo "<option value='" . $row['id'] . "'>" . $row['organization_list_name'] . "</option>";
             }
@@ -25,4 +25,6 @@ function getOrgList()
     </select>
 <?php
 }
+
+getOrgList();
 ?>
