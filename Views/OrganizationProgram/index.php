ae9c87c27564594c00a2cb7bd590b00c439494a6
Lumworker <Lumworker@hotmail.com>
2019-04-11 18:02:10 +0700

    Modol Pic reload

--- Views/OrganizationProgram/index.php
+++ Views/OrganizationProgram/index.php
@@ -108,7 +108,7 @@
         padding: 0 !important;
         background-color: rgb(0, 0, 0);
         /* Fallback color */
-        background-color: rgba(0, 0, 0, 0.4);
+        background-color: rgba(0, 0, 20, 0.4);
         /* Black w/ opacity */
     }
 
@@ -156,7 +156,7 @@
                                         <div class="col-12 d-flex align-items-center justify-content-center">
 
                                             <div class="modal" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                <div class="modal-dialog" role="document">
+                                                <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
                                                     <div class="modal-content">
                                                         <div class="modal-body">
                                                             <img src="../../Asset/Images/gif-To-not-Bg-Th.gif" style="width:90%" alt="">
