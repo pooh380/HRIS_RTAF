322e4a9878c1f9ae120d405f20fe2227b9e98cb1
pooh <poohivaoth@gmail.com>
2019-04-13 14:16:36 +0700

    no message

--- Model/OrganizationProgram/getOrgList.php
+++ Model/OrganizationProgram/getOrgList.php
@@ -4,7 +4,7 @@ require_once "../../config.php";
 
 $orgTypeId = isset($_POST['orgTypeId']) ? $_POST['orgTypeId'] : "";
 
-$sql = " SELECT id,organization_list_name,organization_type_id,IsActive FROM organization_list WHERE organization_type_id = $orgTypeId and IsActive = 1 and id = 49 ; ";
+$sql = " SELECT id,organization_list_abbr_name,organization_type_id,IsActive FROM organization_list WHERE organization_type_id = $orgTypeId and IsActive = 1 and id = 49 ; ";
 $params = array();
 $options = array("Scrollable" => SQLSRV_CURSOR_KEYSET);
 $query = sqlsrv_query($conn, $sql, $params, $options);
@@ -12,7 +12,7 @@ $rows = sqlsrv_num_rows($query);
 
 if ($rows > 0) {
     while ($result = sqlsrv_fetch_array($query)) {
-        echo "<option value='" . $result['id'] . "'>" . $result['organization_list_name'] . "</option>";
+        echo "<option value='" . $result['id'] . "'>" . $result['organization_list_abbr_name'] . "</option>";
     }
 }else{
     echo "<option value=\"\">ไม่มีฐานะหน่วยในโครงสร้าง</option>";
