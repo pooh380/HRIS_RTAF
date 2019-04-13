3dc4bb115a947a757a280ebb24cfdada4be997f1
pooh <poohivaoth@gmail.com>
2019-04-13 17:18:51 +0700

    Prefix_skill/detail.php เมนูปกติแล้ว

--- Views/Prefix_skill/detail.php
+++ Views/Prefix_skill/detail.php
@@ -18,12 +18,12 @@
   <link rel="stylesheet" type="text/css" href="../../app-assets/css/core/colors/palette-gradient.css">
   <link rel="stylesheet" type="text/css" href="../../app-assets/css/plugins/forms/switch.css">
   <link rel="stylesheet" type="text/css" href="../../app-assets/css/plugins/forms/checkboxes-radios.css">
-  <link rel="stylesheet" type="text/css" href="../../assets/css/style.css">
+  <link rel="stylesheet" type="text/css" href="../../asset/css/style.css">
   <!-- END Page Level CSS-->
 
-
-  <!-- END Custom CSS-->
+  <!-- menu -->
   <?php include '../include/menu.php'; ?>
+  
 
   <style>
       /* ol > li > a {color:#222233;} */
@@ -270,29 +270,12 @@
       </div>
   </section>
 
-
-  <script>
-      $(document).ready(function() {
-          $('.dropdown-submenu a.test').on("click", function(e) {
-              $(this).next('ul').toggle();
-              e.stopPropagation();
-              e.preventDefault();
-          });
-      });
-  </script>
-  <!-- BEGIN VENDOR JS-->
-  <script src="../../app-assets/vendors/js/vendors.min.js" type="text/javascript"></script>
-  <script src="../../app-assets/vendors/js/vendors.min.js" type="text/javascript"></script>
   <!-- BEGIN VENDOR JS-->
   <!-- BEGIN PAGE VENDOR JS-->
   <script src="../../app-assets/vendors/js/forms/toggle/switchery.min.js" type="text/javascript"></script>
   <script src="../../app-assets/vendors/js/extensions/dragula.min.js" type="text/javascript"></script>
   <!-- END PAGE VENDOR JS-->
 
-  <!-- BEGIN MODERN JS-->
-  <script src="../../app-assets/js/core/app-menu.js" type="text/javascript"></script>
-  <script src="../../app-assets/js/core/app.js" type="text/javascript"></script>
-  <script src="../../app-assets/js/scripts/customizer.js" type="text/javascript"></script>
   <script src="../../app-assets/vendors/js/forms/icheck/icheck.min.js" type="text/javascript"></script>
   <!-- END MODERN JS-->
 
@@ -304,4 +287,7 @@
 
 
 
-  <?php include '../include/footer.php'; ?>
+  <!-- footer -->
+  <?php
+  include "../include/footer.php";
+  ?> 
