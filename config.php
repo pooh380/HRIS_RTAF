64aebf9fb1ae879cf0d90c23669998ebe42ff655
Lumworker <Lumworker@hotmail.com>
2019-04-18 11:07:06 +0700

    no message

--- config.php
+++ config.php
@@ -1,13 +1,24 @@
 
 <?php
 
-$serverName = "45.77.249.35"; //serverName\instanceName ถ้าฐานข้อมูลอยู่ในเครื่องเราใช้ localhost 
+// $serverName = "45.77.249.35"; //serverName\instanceName ถ้าฐานข้อมูลอยู่ในเครื่องเราใช้ localhost 
+// $serverName = "45.77.249.35"; //serverName\instanceName ถ้าฐานข้อมูลอยู่ในเครื่องเราใช้ localhost 
+// $connectionInfo = array(
+    // "Database" => "HRIS",
+    // "UID" => "sa",
+    // "PWD" => "1020304050",
+    // "MultipleActiveResultSets"=>true,
+    // "CharacterSet"  => 'UTF-8'
+
+    $serverName = "10.243.0.9\MSSQLSERVER01";
 $connectionInfo = array(
     "Database" => "HRIS",
     "UID" => "sa",
-    "PWD" => "1020304050",
+    "PWD" => "2123121254",
     "MultipleActiveResultSets"=>true,
     "CharacterSet"  => 'UTF-8'
+
+
 );
  
 $conn = sqlsrv_connect( $serverName, $connectionInfo,);
@@ -16,6 +27,6 @@ if ( $conn ) {
     // echo "Connection established.<br />";
 } else {
     echo "Connection could not be established.<br />";
-    die( print_r( sqlsrv_errors(), true));
+    die( print_r( sqlsrv_errors(), tdrue));
 }
 ?>
