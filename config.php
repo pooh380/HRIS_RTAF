bcb3a81a05079a151527b27849fa920e904ca124
Lumworker <Lumworker@hotmail.com>
2019-04-18 11:12:40 +0700

    conFix .09

--- config.php
+++ config.php
@@ -4,29 +4,27 @@
 // $serverName = "45.77.249.35"; //serverName\instanceName ถ้าฐานข้อมูลอยู่ในเครื่องเราใช้ localhost 
 // $serverName = "45.77.249.35"; //serverName\instanceName ถ้าฐานข้อมูลอยู่ในเครื่องเราใช้ localhost 
 // $connectionInfo = array(
-    // "Database" => "HRIS",
-    // "UID" => "sa",
-    // "PWD" => "1020304050",
-    // "MultipleActiveResultSets"=>true,
-    // "CharacterSet"  => 'UTF-8'
+// "Database" => "HRIS",
+// "UID" => "sa",
+// "PWD" => "1020304050",
+// "MultipleActiveResultSets"=>true,
+// "CharacterSet"  => 'UTF-8'
 
-    $serverName = "10.243.0.9\MSSQLSERVER01";
+$serverName = "10.243.0.9\MSSQLSERVER01";
 $connectionInfo = array(
     "Database" => "HRIS",
     "UID" => "sa",
     "PWD" => "2123121254",
-    "MultipleActiveResultSets"=>true,
+    "MultipleActiveResultSets" => true,
     "CharacterSet"  => 'UTF-8'
-
-
 );
- 
-$conn = sqlsrv_connect( $serverName, $connectionInfo,);
- 
-if ( $conn ) {
+
+$conn = sqlsrv_connect($serverName, $connectionInfo,);
+
+if ($conn) {
     // echo "Connection established.<br />";
 } else {
     echo "Connection could not be established.<br />";
-    die( print_r( sqlsrv_errors(), tdrue));
+    die(print_r(sqlsrv_errors(), tdrue));
 }
 ?>
