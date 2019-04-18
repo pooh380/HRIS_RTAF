ec9a93c2b5701f8c730cc7d4176a9e3f3d430dfe
pooh <poohivaoth@gmail.com>
2019-04-18 13:07:44 +0700

    no message

--- dbTest.sql
+++ dbTest.sql
@@ -11,12 +11,6 @@ SELECT table_catalog [database], table_schema [schema], table_name name, table_t
 FROM INFORMATION_SCHEMA.TABLES
 GO
 
+use HRIS
 SELECT id, seq_no, prefix_code, prefix_name, prefix_abbr_name, is_active FROM ganeral_perfix
 
-SELECT id, organization_part_code, organization_part_name, organization_part_abbr_name FROM organization_part WHERE organization_part_abbr_name Like '%m%'
-
-SELECT id, organization_part_code, organization_part_name, organization_part_abbr_name  FROM organization_part WHERE id = 6
-
-SELECT id, organization_part_code, organization_part_name, organization_part_abbr_name, status FROM organization_part WHERE 1=1 AND (organization_part_name Like N'%ส%' OR organization_part_abbr_name Like N'%ส%')
-
-SELECT * from ganeral_origins
