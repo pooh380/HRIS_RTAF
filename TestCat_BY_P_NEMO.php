e10474724e4cd3c4d3845215effa838e6136022f
Lumworker <lumworker@hotmail.com>
2019-04-29 20:17:11 +0700

    ชลบุรีศรีอยุธยา

--- TestCat_BY_P_NEMO.php
+++ TestCat_BY_P_NEMO.php
@@ -377,11 +377,12 @@
 					foreach ($category['parent_cats'][$parent] as $cat_id) {
 							if (!isset($category['parent_cats'][$cat_id])) {
 									$html .= "<div>";
-									$html .= "<li style='display: list-item;' class='mjs-nestedSortable-leaf' id='menuItem_".$cat_id."'>";
-									$html .= "<a href='".$cat_id."'>" . $category['categories'][$cat_id]['OrgStrucName'] . "</a></div></li> \n";
+									$html .= "<li style='display: list-item;' class='mjs-nestedSortable-leaf' id='menuItem_".$cat_id."'></div>";
+									$html .= "<div>><a href='".$cat_id."'>" . $category['categories'][$cat_id]['OrgStrucName'] . "</a></div></li> \n";
 								}
 							if (isset($category['parent_cats'][$cat_id])) {
-									$html .= "<li style='display: list-item;' class='mjs-nestedSortable-leaf' id='menuItem_".$cat_id."'><div class='menuDiv'>" . $category['categories'][$cat_id]['OrgStrucName'] . "</div> \n";
+									$html .= "<li style='display: list-item;' class='mjs-nestedSortable-leaf' id='menuItem_".$cat_id."'>";
+									$html .= " <div class='menuDiv'>" . $category['categories'][$cat_id]['OrgStrucName'] . "</div> \n";
 									$html .= getCategories($cat_id, $category);
 									$html .= "</li> \n";
 								}
