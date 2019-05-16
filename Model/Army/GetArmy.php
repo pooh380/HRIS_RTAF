a4fe06f2ef41bd88c4fb7c58cb29e31747084b28
Lumworker <lumworker@hotmail.com>
2019-05-16 10:29:23 +0700

    Armเสร็จ

--- Model/Army/GetArmy.php
+++ Model/Army/GetArmy.php
@@ -1,6 +1,6 @@
 <?php
 
-// error_reporting(0);
+error_reporting(0);
 
 require_once "../../config.php";
 
@@ -9,34 +9,31 @@ $col =array(
     0   =>  'HrtArmId',
     1   =>  'HrtArmName',
     2   =>  'HrtArmAbbr',
-    3   =>  'HrtArmActive',
+    3   =>  'HrtArmActive'
 ); 
 
 
-$sql =" SELECT HrtArmId, HrtArmName, HrtArmAbbr, HrtArmActive FROM HrtArm ; ";
+$sql =" SELECT HrtArmId, HrtArmName, HrtArmAbbr, HrtArmActive FROM HrtArm  ";
 $params = array();
 $options = array("Scrollable" => SQLSRV_CURSOR_KEYSET);
 $query = sqlsrv_query($conn, $sql, $params, $options);
-
 $totalData = sqlsrv_num_rows($query);
 $totalFilter = $totalData;
 
-
-
 $sql = "  SELECT HrtArmId, HrtArmName, HrtArmAbbr, HrtArmActive FROM HrtArm WHERE 1=1 ";
 if (!empty($request['search']['value'])) {
-    $sql .= " AND (HrtArmId Like N'%" . $request['search']['value'] . "%'); ";
+    $sql .= " AND (HrtArmId Like N'%" . $request['search']['value'] . "%') ";
     $query = sqlsrv_query($conn, $sql, $params, $options);
     $totalData = sqlsrv_num_rows($query);
 }
-// echo $sql;
-$data = array();
+$data=array();
+
 
 while($row=sqlsrv_fetch_array($query)){
     $subdata=array();
     $subdata[] = '
     <a href="../../Views/Armys/edit.php?id=' . $row[0] . '"><i class="la la-pencil-square-o" style="color:#0f1733"; font-size:30px;" id="' . $row[0] . '"></i></a>
-    <a href=""><i class="la la-trash-o" style="color:#0f1733"; font-size:30px;" onclick="deleteOrgType(' . $row[0] . ')" id="' . $row[0] . '"></i></a>
+    <a href=""><i class="la la-trash-o" style="color:#0f1733"; font-size:30px;" onclick="deleteArmy(' . $row[0] . ')" id="' . $row[0] . '"></i></a>
     '; //id
     $subdata[] = $row[0]; //id
     // $subdata[] = $row[1]; //id
@@ -46,7 +43,7 @@ while($row=sqlsrv_fetch_array($query)){
         $subdata[] = '<i class="la la-toggle-off" style="color: red;font-size:30px;"></i>';
     }else{
         $subdata[] = '<i class="la la-toggle-on" style="color: green; font-size:30px; "></i>';
-    }//status   
+    }//status       
     $data[]=$subdata;
 }
 $json_data=array(
@@ -61,6 +58,6 @@ $json_data=array(
 
 echo json_encode($json_data);
 
-// sqlsrv_close($conn);
+sqlsrv_close($conn);
 
 ?>
