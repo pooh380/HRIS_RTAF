09d925cb6d3b2d3e35e337831d159685d1329692
pooh <poohivaoth@gmail.com>
2019-05-17 08:59:31 +0700

    no message

---
+++ Controllers/RecruitingOfficerController.js
@@ -0,0 +1,194 @@
+function update() {
+
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "",
+        data: {},
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+
+}
+
+function uploadImages() {
+
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "",
+        data: {},
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+
+}
+
+function search() {
+
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "",
+        data: {},
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+
+}
+
+function reqChange() {
+
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "",
+        data: {},
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+
+} //คำข้อต่างๆ ในส่วนของคนข้อ function getReqChange //ดึงคำข้อมาให้สัสดี 
+
+
+function saveOnHistory() {
+
+    $$.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "",
+        data: {},
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+
+} //เก็บสัสดี 
+
+
+function saveHistoryPerson() {
+
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "",
+        data: {},
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+
+} //เก็บลงประวัติราชการ
+
+
+function getHistoryPerson() {
+
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "",
+        data: {},
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+
+} // แก้ไขสิ่งต่างๆๆ ในส่วนของสัสดี 
+
+
+//การขึ้นทะเบียนทหาร 
+function searchPersonNotArmy() {
+
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "",
+        data: {},
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+
+} //id ขึ้นเลข 8 นักเรียนทหาร มีชื่อ ยศเป็นนักเรีบน แยกกับข้าราชการ ไม่มียศ มี report word query มาจาก ประวัติ ชาย 
+
+
+function saveArmy() {
+
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "",
+        data: {},
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+
+} // 
+
+
+function reportDocument() {
+
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "",
+        data: {},
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+
+} // คนที่ขึ้นทะเบียนแล้ว ตามคำของ 
+
+
+function calculatorAgeOfArmy() {
+
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: "",
+        data: {},
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+
+} //คำนวนวันที่เป็นชั้นกองหนุน
